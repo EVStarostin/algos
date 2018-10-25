db1cb96dad7121605f91cfdf8ea53ae3ff9fa317
Evgenii Starostin <e.starostin89@gmail.com>
2018-10-26 01:14:05 +0300

    подправил бинарный поиск

--- src/binary_search.test.js
+++ src/binary_search.test.js
@@ -1,14 +1,15 @@
 const { expect } = require('chai');
 const { binarySearch } = require('./binary_search');
 
-describe('binary_search', () => {
+describe('binarySearch', () => {
   const myList = [1, 3, 5, 7, 9];
-  it('должна возвращать позицию 1 для значения 3 в массиве [1, 3, 5, 7, 9]', () => {
+  
+  it(`должна возвращать позицию 1 для значения 3 в массиве ${myList}`, () => {
     const res = binarySearch(myList, 3);
     expect(res).to.equal(1, `${res} !== 1`);
   });
 
-  it('должна возвращать null для значения -1 в массиве [1, 3, 5, 7, 9]', () => {
+  it(`должна возвращать null для значения -1 в массиве ${myList}`, () => {
     const res = binarySearch(myList, -1);
     expect(res).to.be.a('null', `${res} !== null`);
   });
