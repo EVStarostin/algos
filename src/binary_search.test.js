53fe3e74fdc809803a061aaf19b0c61e5ad29756
Evgenii Starostin <e.starostin89@gmail.com>
2018-10-26 01:17:36 +0300

    прошелся линтером по файлам

--- src/binary_search.test.js
+++ src/binary_search.test.js
@@ -3,7 +3,7 @@ const { binarySearch } = require('./binary_search');
 
 describe('binarySearch', () => {
   const myList = [1, 3, 5, 7, 9];
-  
+
   it(`должна возвращать позицию 1 для значения 3 в массиве ${myList}`, () => {
     const res = binarySearch(myList, 3);
     expect(res).to.equal(1, `${res} !== 1`);
--- src/selection_sort.js
+++ src/selection_sort.js
@@ -1,4 +1,23 @@
 /**
+ * @description Поиск наименьшего элемента в массиве
+ * @param {number[]} arr - Массив, в котором выполняется поиск
+ * @returns {number} - Индекс наименьшего элемента
+ */
+function findSmallest(arr) {
+  let smallest = arr[0];
+  let smallestIndex = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallestIndex = i;
+    }
+  }
+
+  return smallestIndex;
+}
+
+/**
  * @description Сортировка выбором выполняется за время O(n ** 2)
  * @param {number[]} arr - Неотсортированный массив
  * @returns {number[]} Отсортированный массив
@@ -11,25 +30,6 @@ function selectionSort(arr) {
   }
 
   return newArr;
-}
-
-/**
- * @description Поиск наименьшего элемента в массиве 
- * @param {number[]} arr - Массив, в котором выполняется поиск
- * @returns {number} - Индекс наименьшего элемента
- */
-function findSmallest(arr) {
-  let smallest = arr[0];
-  let smallestIndex = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < smallest) {
-      smallest = arr[i];
-      smallestIndex = i;
-    }
-  }
-
-  return smallestIndex;
 }
 
 module.exports = { selectionSort };
